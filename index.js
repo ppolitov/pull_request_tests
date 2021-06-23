5a5ad1f7bd3c2b747a68cc62284c69afaf851808
Pavel Politov <politov@meetsidekick.com>
2021-06-23 14:58:25 +0400

    Check successful builds

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ async function run() {
     const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/');
     const pull_number = payload.pull_request.number;
     console.log(`Inputs: pull:${pull_number} owner:${owner} repo:${repo}`);
+    console.log('Teamcity:', teamcityToken);
 
     const octokit = github.getOctokit(token);
     const { data: reviews } = await octokit.pulls.listReviews(
@@ -38,11 +39,32 @@ async function run() {
       return;
 
     const branch = `pull/${pull_number}`;
-    const url = `${TEAMCITY_API}/app/rest/buildQueue`;
     const headers = {
       'Authorization': `Bearer ${teamcityToken}`,
       'Content-Type': 'application/json',
+      'Accept': 'application/json',
     };
+
+    // Checking if there are successful builds
+    const locator = `branch:${branch},state:any,count:9`;
+    let res = await axios({
+      url: `${TEAMCITY_API}/app/rest/builds/?locator=${locator}`,
+      method: 'GET',
+      headers,
+    });
+    if (res.status === 200) {
+      const builds = res.data.build.filter((b) => b.status === 'SUCCESS');
+      if (builds.length > 0) {
+        console.log('Already exist successful builds. Skipping...');
+        return;
+      }
+    } else {
+      console.error('Error fetching pull request builds:', res.status, res.statusText);
+      return;
+    }
+
+    // Starting builds
+    const url = `${TEAMCITY_API}/app/rest/buildQueue`;
     for (let buildType in BUILD_TYPES) {
       let body = JSON.stringify({
         branchName: branch,
@@ -56,7 +78,6 @@ async function run() {
         console.warn(`Failed to start build ${buildType}:`, res.statusText);
       }
     }
-
   } catch (error) {
     core.setFailed(error.message);
   }
